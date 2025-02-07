20b330a998defa896f4c41ccb512209d9b1fca3a
8aty <unconfigured@null.spigotmc.org>
2025-02-07 14:24:55 +0100

    Git Upload

---
+++ src/main/java/cc/tomy/oreLogger/OreLogger.java
@@ -0,0 +1,17 @@
+package cc.tomy.oreLogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OreLogger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
